4ac26bb529fdf283671e0e9c71d9553d76358ecd
avionics18 <avimurmu18@gmail.com>
2023-02-07 11:58:46 +0530

    Project Setup Done: Build Tasks, Sass Boilerplate, JS Bundling

---
+++ README.md
@@ -0,0 +1,13 @@
+Learnings
+=========
+
+1. Why we use or why popular frameworks go with min-width media queries rather than max-width query even though max-width seems more natural to mind?
+
+Ans: The reason is because mobile styles need very little css as they are stacked-up right! The complexity increases when we move to larger devices, so code should also scale according to that => write extra code when you need it, Right!
+
+But in the case of max-width what happens - you write all the complex code for that element and using max-width you keep removing the extra properties => uneccessary code for smaller devices.
+
+2. `node-sass` is **Depricated**, don't use it, Use the `sass` package instead. But what I understood is always do the research, then only proceed.
+```js
+const sass = require("gulp-sass")(require("sass"));
+``
---
+++ gulpfile.js
@@ -0,0 +1,66 @@
+// Initialize Modules
+const { src, dest, watch, series } = require("gulp");
+const sass = require("gulp-sass")(require("sass"));
+const postcss = require("gulp-postcss");
+const autoprefixer = require("autoprefixer");
+const cssnano = require("cssnano");
+const babel = require("gulp-babel");
+const terser = require("gulp-terser");
+const browsersync = require("browser-sync").create();
+
+// Sass Task
+function scssTask() {
+	return src('app/scss/style.scss', { sourcemaps: true })
+					.pipe(sass())
+					.pipe(postcss([autoprefixer(), cssnano()]))
+					.pipe(dest('dist', { sourcemaps: '.' }));
+}
+
+// Javascript Task
+function jsTask() {
+	return src('app/js/script.js', { sourcemaps: true })
+					.pipe(babel({presets: ['@babel/preset-env']}))
+					.pipe(terser())
+					.pipe(dest('dist', { sourcemaps: '.' }));
+}
+
+// BrowserSync
+function bsServe(cb) {
+	browsersync.init({
+		server: {
+			baseDir: '.',
+		},
+		notify: {
+			styles: {
+				top: 'auto',
+				bottom: '0',
+			},
+		},
+	});
+
+	cb();
+}
+
+function bsReload(cb) {
+	browsersync.reload();
+	cb();
+}
+
+
+// ===GULP WORKFLOW===
+// Watch Task
+function watchTask() {
+	watch('*.html', bsReload);
+	watch(
+		['app/scss/**/*.scss', 'app/js/**/*.js'],
+		series(scssTask, jsTask, bsReload)
+	);
+}
+
+// Default Gulp Task
+exports.default = series(
+	scssTask,
+	jsTask,
+	bsServe,
+	watchTask
+);
